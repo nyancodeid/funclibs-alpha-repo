c458596d926bb13dde77bb92a63834e263acf438
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2017-08-05 11:24:39 +0700

    new library make-stress by wildanaunur

---
+++ wildanaunur/make-stressfc9a8.js
@@ -0,0 +1,22 @@
+/*
+ *  make-stress
+ *  Fascade String Support
+ *  
+ *  @author wildanaunur<wildanaunur@gmail.com>
+ */ 
+
+/**
+ *  rupiah-to-int
+ *  Convert Rupiah Format to Integer
+ * 
+ *  @author ryanaunur<ryan3@dev.com>
+ *  @version 1.0.0
+ */
+
+function rupiahToInt() {
+
+}
+
+/*
+ * Generate with FuncLibs<funclibs.com> 
+ */
