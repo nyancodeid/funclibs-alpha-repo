d5dc1155c13b9c96248cf9b0f0cf709fcf488d65
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2017-08-05 11:29:29 +0700

    new library my-libriray by wildanaunur

---
+++ wildanaunur/my-libriray.385cd.js
@@ -0,0 +1,50 @@
+/*
+ *  my-libriray
+ *  My Custom library
+ *  
+ *  @author wildanaunur<wildanaunur@gmail.com>
+ */ 
+
+/**
+ *  rupiah-to-int
+ *  Convert Rupiah Format to Integer
+ * 
+ *  @author ryanaunur<ryan3@dev.com>
+ *  @version 1.0.0
+ */
+
+function rupiahToInt() {
+
+}
+
+/**
+ *  bitcoin-to-idr
+ *  Bitcoin converter curency
+ * 
+ *  @author ryanaunur<ryan3@dev.com>
+ *  @version 1.0.3
+ */
+
+var api = '/';
+var jobs = false;
+
+function bitcoinToIdr() {
+
+}
+
+/**
+ *  token-builder
+ *  awesome function
+ * 
+ *  @author ryanaunur<ryan3@dev.com>
+ *  @version 1.0.0
+ */
+
+function makeToken(token, callback) {
+    var output = atob(token);
+    callback(null, output);
+}
+
+/*
+ * Generate with FuncLibs<funclibs.com> 
+ */
